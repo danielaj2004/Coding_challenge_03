e9772f1bc457d0754026c8e1b71d5e3302f3115f
danielaj2004 <danielajoya12@gmail.com>
2025-02-01 19:57:35 -0500

    Task 3 - Employee Performance Tracking

--- cc_3.js
+++ cc_3.js
@@ -15,3 +15,14 @@ console.log(order); // [52,68,89,39,96]
 let totalOrder = order.reduce((total, orders) => total + orders, 0); //calculate  the total number of all orders
 console.log("total number of orders: ", totalOrder); // Output: total number of orders
 console.log("Updated number of orders: ", order); // Output: the updated array of orders
+
+// Task 3
+let employee = {
+    name: "Tina",
+    role: "data analyist",
+    performanceScore: 80,
+    isActive: true 
+} // Object employee
+employee.performanceScore = 90; // Outout: increase the performance score by 10
+employee.promotionEligible = true; // add a new proptery to the object
+console.log("Updated employee: ", employee); // Output: the updated object
