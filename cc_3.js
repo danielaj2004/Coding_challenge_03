6da4496cae13f97616cf66b87c717c5482def143
danielaj2004 <danielajoya12@gmail.com>
2025-02-01 19:28:17 -0500

    Task 2 - Modifying Customer orders

--- cc_3.js
+++ cc_3.js
@@ -6,3 +6,12 @@ console.log(price); // Output: ["10", "12", "14", "16", "18", "20"]
 let removedItem = price.shift();
 console.log("removed price", removedItem); // Output: "10"
 console.log("updated prices", price); // Output: ["12", "14", "16", "18", "20"]
+
+// Task 2
+let order = [52,68,84,39,96];
+console.log("order number", order); // Output: [52,68,84,39,96]
+order[2] += 5; // add 5 to the third order
+console.log(order); // [52,68,89,39,96]
+let totalOrder = order.reduce((total, orders) => total + orders, 0); //calculate  the total number of all orders
+console.log("total number of orders: ", totalOrder); // Output: total number of orders
+console.log("Updated number of orders: ", order); // Output: the updated array of orders
